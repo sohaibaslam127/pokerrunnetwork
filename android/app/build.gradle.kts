2d0b983a09af53f50aeedefa9b4cd865a6211550
siman302 <sohaibaslam127@gmail.com>
2025-12-23 22:28:41 +0500

    fixed android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,14 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.techbolic.pokernetwork"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,20 +21,16 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.techbolic.pokernetwork"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/java/com/techbolic/pokerrunnetwork/MainActivity.java
+++ android/app/src/main/java/com/techbolic/pokerrunnetwork/MainActivity.java
@@ -1,4 +1,4 @@
-package com.techbolic.pokerrunnetwork;
+package com.techbolic.pokernetwork;
 
 import io.flutter.embedding.android.FlutterActivity;
 
