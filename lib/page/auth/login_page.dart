d62c7119c2ba76234c973cb73aca87bdbf85f3f9
sohaibaslam127 <sohaibaslam127@gmail.com>
2026-04-21 21:02:53 +0500

    remove the apple and google login

--- lib/page/auth/login_page.dart
+++ lib/page/auth/login_page.dart
@@ -142,35 +142,16 @@ class _LoginPageState extends State<LoginPage> {
                                   color: MyColors.secondary,
                                 ),
                               ),
+                              SizedBox(height: 2.5.h),
                             ],
                           ),
-                          SizedBox(height: 2.h),
+                          SizedBox(height: 2.5.h),
                           customButon(
                             isIcon: false,
                             btnText: "Sign In",
                             onTap: signIn,
                           ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: customButon(
-                                  isIcon: true,
-                                  icon: "assets/icons/google.png",
-                                  btnText: "Google",
-                                  onTap: signIn,
-                                ),
-                              ),
-                              Expanded(
-                                child: customButon(
-                                  isIcon: true,
-                                  btnText: "Apple",
-                                  icon: "assets/icons/apple.png",
-                                  onTap: signIn,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 2.h),
+                          SizedBox(height: 3.h),
                           Center(
                             child: onPress(
                               ontap: () {
@@ -198,6 +179,7 @@ class _LoginPageState extends State<LoginPage> {
                               ),
                             ),
                           ),
+                          SizedBox(height: 2.h),
                         ],
                       ),
                     ),
